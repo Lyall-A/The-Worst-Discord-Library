a4e5066e7ba36a03d2ae5300ef6efad2dc5274b3
Lyall <me@lyall.lol>
2024-12-14 15:47:47 +0000

    nein

--- src/imports.js
+++ src/imports.js
@@ -1,7 +1,7 @@
 const dgram = require("node:dgram");
 const crypto = require("node:crypto");
 const child_process = require("node:child_process");
-const WebSocket = require("ws");
+// const WebSocket = require("ws");
 
 module.exports = {
     fetch,
